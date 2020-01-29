5654c02588ed18fef9366362b8422cec75696503
Yolea Mayers <yoleamayers@Yoleas-Air.fios-router.home>
2020-01-29 18:58:06 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,19 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped" + cardOne);
+cards = ['queen', 'queen', 'king', 'king'];
+cardsInPlay = [];
+let cardOne = cards[3];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
+
