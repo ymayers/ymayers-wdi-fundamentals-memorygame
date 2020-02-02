a90067f4045c75b8f125b6415747a9c81a06ccb3
Yolea Mayers <yoleamayers@Yoleas-Air.fios-router.home>
2020-02-02 15:11:00 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,27 +24,40 @@ const cards = [
 cardsInPlay = [];
 
 function checkForMatch() {
+	
 	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	}
 	else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 		}
 }
 }
 
-function flipCard(cardId) {
-	
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
+function flipCard() {
+
+let cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+this.setAttribute('src', cards[cardId].cardImage);
+
+ 
 checkForMatch();
-
 }
-flipCard(0);
-flipCard(2);
 
 
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src',"images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+}
+createBoard();
 
